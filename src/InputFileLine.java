ab00c4c4be85de4c5d1a9f59d7d5560a672f8f88
omar222os <124836096+omar222os@users.noreply.github.com>
2025-05-17 21:07:48 +0300

    Delete src directory

--- src/InputFileLine.java
+++
@@ -1,9 +0,0 @@
-public class InputFileLine {
-    String Location;
-    String LineNumber ;
-    String Delimeter = "      ";
-    String Label;
-    String menmonic;
-    String Operand ;
-    String Comment;
-}
